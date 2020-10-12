13b1442669242d7e82668874c6a299da713274e6
Smriti Gupta <55406606+SmritiUMD@users.noreply.github.com>
2020-10-12 11:07:55 -0400

    Update 96UniqueBinarySearchTrees.cpp

--- Leetcode Questions/96UniqueBinarySearchTrees.cpp
+++ Leetcode Questions/96UniqueBinarySearchTrees.cpp
@@ -1,6 +1,7 @@
 /******************************************************************************
 @author-Smriti Gupta
 @date created-October 12,2020
+Dynamic programming- Bottom up Approach
 
 *******************************************************************************/
 
@@ -15,7 +16,8 @@ public:
         vector<int>G(n+1,0);
         G[0]=1;
         G[1]=1;
-        
+        // for every number we will calculate all the possibilities
+        // we will start from taking first number as root
         for(int i=2;i<=n;i++){
             for(int j=1;j<=i;j++){
                 G[i]+=G[j-1]*G[i-j];
