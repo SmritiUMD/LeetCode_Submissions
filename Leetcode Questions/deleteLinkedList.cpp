7acfca5afb51e7f1b4e9388f0f9a2ac86259afa2
Smriti Gupta <55406606+SmritiUMD@users.noreply.github.com>
2020-12-08 23:32:58 -0500

    Create deleteLinkedList.cpp

---
+++ Leetcode Questions/deleteLinkedList.cpp
@@ -0,0 +1,64 @@
+/******************************************************************************
+@author- Smriti Gupta
+@date created- Dec 8,20
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+class ListNode{
+    public:
+    int val;
+    ListNode* next;
+    
+};
+//takes pointer to pointer to the head of the List
+//it pushes a node in the front of the list
+void push(ListNode** head, int new_data){
+    //allocating new node
+    ListNode* new_node= new ListNode();
+    //putting the data
+    new_node->val=new_data;
+    //link old list off the new node
+    new_node->next=(*head);
+    // now the head pointer will point to new_node
+    *head=new_node;
+}
+
+void printList(ListNode* head){
+    while(head){
+        cout<<"<-"<<head->val;
+        head=head->next;
+    }
+    cout<<endl;
+    
+}
+
+void deleteList(ListNode* head){
+    ListNode* current = head;
+    while(current){
+        ListNode* next=current->next;
+        free(current);
+        current =next;
+        
+    }
+    cout<<"deleted";
+}
+
+int main()
+{
+    ListNode* head=NULL;
+    push(&head, 1);
+    push(&head, 2);
+    push(&head,5);
+    push(&head, 7);
+    push(&head, 10);
+    
+    printList(head);
+    deleteList(head);
+    
+
+    return 0;
+}
