67be32dbdb17d3e7d32e70d10aee00c1aaecbfeb
Smriti Gupta <55406606+SmritiUMD@users.noreply.github.com>
2021-01-20 14:07:47 -0500

    Update tree_from_in_post_traversal.cpp

--- CppClasses/tree/tree_from_in_post_traversal.cpp
+++ CppClasses/tree/tree_from_in_post_traversal.cpp
@@ -9,8 +9,6 @@
 
 using namespace std;
 
-
-
  struct TreeNode {
      
      int val;
@@ -22,26 +20,34 @@ using namespace std;
          right=NULL;
          
      }
-     };
+ };
  
  
 class Solution {
+ //defining variables here so that both functions can update them
+ // it removes the need to pass the variable in recursive functions repeatitively
     int post_idx=0;
     int index=0;
     vector<int> postorder;
     vector<int>inorder;
     map<int,int>inorder_idx;
-    
-    TreeNode* helper(int left, int right){
+ 
+ //helper recursive function to build trees from inorder traversal
+ TreeNode* helper(int left, int right){
+     //checking if tree is empty
         if(left>right){
             return NULL;
         }
+     
       int root_val=postorder[post_idx];
+     
+     //finding index of the last value from post in inorder to make subtrees
       int index=inorder_idx[root_val];
+     
+     //creating tree with the last value from postorder
       struct TreeNode* root=new TreeNode(root_val);
       //recursion to make subtrees
         post_idx--;
-        
         root->right=helper(index+1, right);
         root->left=helper(left, index-1);
         
@@ -51,9 +57,11 @@ public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         this->postorder=postorder;
         this->inorder=inorder;
-    
+        //last index of post order vector will be the top root of tree
+     // we will use this root to find left and right subtrees from inorder traversal
         post_idx=postorder.size()-1;
         index=0;
+     //map stores values and indices of inorder traversal
         for(int i=0;i<inorder.size();i++){
             inorder_idx.insert({inorder[i],index});
             index++;
@@ -75,15 +83,8 @@ void printTree(TreeNode* root){
         if(q.empty()){
             cout<<endl;
         }
-        if(curr->left) {
-            q.push(curr->left);
-            index++;
-        }
-        if(curr->right) {
-            q.push(curr->right);
-            index++;
-        }
-        
+        if(curr->left) q.push(curr->left);
+        if(curr->right) q.push(curr->right);
     }
 }
 
