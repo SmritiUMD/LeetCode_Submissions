131f7aac64dbec6c6995debc26c01aeb54d0f388
Smriti Gupta <55406606+SmritiUMD@users.noreply.github.com>
2021-01-23 15:38:56 -0500

    Update 1091ShortestPathinBinaryMatrix.cpp

--- Leetcode Questions/1091ShortestPathinBinaryMatrix.cpp
+++ Leetcode Questions/1091ShortestPathinBinaryMatrix.cpp
@@ -15,10 +15,9 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>grid) {
         //creating a directory of possible moves
         vector<pair<int,int>>dir={{-1,0},{1,0},{0,-1},{0,1},{1,1},{-1,1},{1,-1},{-1,-1}};
-        
         if(grid[0][0]!=0 || grid[grid.size()-1][grid.size()-1]!=0) return -1;
         
-        // cout<<dir[1].first<<dir[1].second;
+     
         
         //BFS
         queue<pair<int,int>>q;
@@ -43,6 +42,57 @@ public:
                     //for every neighbour addiing current grid value +1 to calculate the distance
                     grid[new_x][new_y]=1+distance;    
                     q.push({new_x,new_y});
+                
+                }
+                    
+                
+            }
+        }
+        
+       return -1;
+        
+    }
+};
+
+
+//For Multithreaded Environments, instead of changing the input, it is always a good way to keep a track of visited nodes in additional data structure.
+
+class Solution {
+    private:
+    vector<pair<int,int>>dir={{-1,0},{1,0},{0,-1},{0,1},{1,1},{-1,1},{1,-1},{-1,-1}};
+    
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        if(grid[0][0]!=0 || grid[grid.size()-1][grid.size()-1]!=0) return -1;
+        
+        cout<<dir[1].first<<dir[1].second;
+        
+        //BFS
+        queue<vector<int>>q;
+        grid[0][0]=1;
+        vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size()));
+        visited[0][0]=true;
+   
+        int distance=1;
+        q.push({0,0,distance});
+        
+        while(!q.empty()){
+            auto curr=q.front();
+            q.pop();
+            int row=curr[0];
+            int col=curr [1];
+            distance=curr[2];
+            cout<<distance;
+            if(row==grid.size()-1 && col==grid.size()-1) return distance ;
+            
+            for(int i=0;i<8;i++){
+                int new_x=curr[0]+dir[i].first;
+                int new_y=curr[1]+dir[i].second;
+                
+                if(new_x>=0 && new_x<grid.size() && new_y>=0 && new_y<grid.size() && grid[new_x][new_y]==0 && !visited[new_x][new_y] ){
+                      
+                    q.push({new_x,new_y,1+distance});
+                    visited[new_x][new_y]=true;
                 
                 }
                     
