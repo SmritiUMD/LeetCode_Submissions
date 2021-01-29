d7ad4e1598209d25307fdaac98c639c221bfe6d0
Smriti Gupta <55406606+SmritiUMD@users.noreply.github.com>
2021-01-29 01:10:47 -0500

    Update 253Meeting RoomsII.cpp

--- Leetcode_curated_170/253Meeting RoomsII.cpp
+++ Leetcode_curated_170/253Meeting RoomsII.cpp
@@ -1,3 +1,5 @@
+//Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
 #include <bits/stdc++.h>
 
 class Solution {
