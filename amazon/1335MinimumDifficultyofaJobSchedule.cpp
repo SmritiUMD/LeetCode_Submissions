569da76eb28603db6d06415bb03c454199a4809a
Smriti Gupta <55406606+SmritiUMD@users.noreply.github.com>
2021-02-12 14:20:26 -0500

    Create 1335MinimumDifficultyofaJobSchedule.cpp

---
+++ amazon/1335MinimumDifficultyofaJobSchedule.cpp
@@ -0,0 +1,63 @@
+/******************************************************************************
+@author - Smriti Gupta
+@date Creates- Feb 12, 21
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+    /*cut the arrary into contigous subarrays and return the minimum sum of max of 
+    each subarray.
+    */
+    //declaring private variable to reduce computation time. I was getting time limit exceeded error
+   
+    vector<vector<int>>dp;
+    vector<int>jobDifficulty;
+    private:
+    int helper(int d ,int idx){
+        
+        if(d==1){
+            int max_sum=0;
+            while(idx<jobDifficulty.size()){
+                max_sum=max(max_sum,jobDifficulty[idx++]);
+            }
+            return max_sum;
+        }
+        if(dp[d][idx]!=-1) return dp[d][idx];
+        int max_sum=0;
+        int result=INT_MAX;
+        //I am makind d cuts and storing min of sum of all the max elements
+        for(int i=idx;i<jobDifficulty.size()-d+1;i++){
+            max_sum=max(max_sum, jobDifficulty[i]);
+            
+            result=min(result, max_sum+helper(d-1, i+1));
+        }
+        
+        return dp[d][idx]= result;
+    }
+    
+public:
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        this->jobDifficulty=jobDifficulty;
+        if(d>jobDifficulty.size())
+            return -1;
+      
+        vector<vector<int>>x(d+1, vector<int>(jobDifficulty.size(),-1));
+        dp=x;
+    
+        
+        return helper(d, 0);
+        
+        
+    }
+};
+
+int main()
+{
+    vector<int>jobDifficulty = {6,5,4,3,2,1};
+    int d = 2;
+
+    return 0;
+}
