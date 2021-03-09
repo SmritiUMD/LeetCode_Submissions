c8133dc78b7c33c9c6edb90511c72fe89e27ee00
Smriti Gupta <55406606+SmritiUMD@users.noreply.github.com>
2021-03-09 13:20:58 -0500

    Update 102BinaryTreeLevelOrderTraversal.cpp

--- amazon/102BinaryTreeLevelOrderTraversal.cpp
+++ amazon/102BinaryTreeLevelOrderTraversal.cpp
@@ -21,17 +21,7 @@ struct TreeNode{
     }
 };
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 // class Solution {
 //     vector<vector<int>>results;
 //     void helper(TreeNode* root, int level){
