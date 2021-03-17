3c28adee517d03f4af452339298a3ef1aba70773
Smriti Gupta <55406606+SmritiUMD@users.noreply.github.com>
2021-03-16 22:32:50 -0400

    Create 323. Number of Connected Components in an Undirected Graph.cpp

---
+++ amazon/unionFind/323. Number of Connected Components in an Undirected Graph.cpp
@@ -0,0 +1,101 @@
+/******************************************************************************
+@author - Smriti Gupta
+@date created - March 16,21
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ class DisjointSet{
+        private:
+        vector<int>parents;
+        vector<int>weights;
+        public:
+     
+       DisjointSet(int N){
+            //setting initial parent node to itself
+            // setting initial weights to 1
+            this->parents.resize(N+1,0);
+            this->weights.resize(N+1,0);
+            for(int i=1;i<=N;i++){
+                this->parents[i]=i;
+                this->weights[i]=1;
+            }
+        }
+        void Union(int a, int b){
+            int rootA=Find(a);
+            int rootB=Find(b);
+            //if both a and b have same root(they belong to same set), 
+            //no need to take union
+            if(rootA==rootB)
+                return ;
+            if(this->weights[rootA]>this->weights[rootB]){
+            /// In case rootA is having more weight
+            // 1. Make rootA as the parent of rootB
+            // 2. Increment the weight of rootA by rootB's weight
+                this->parents[rootB]=rootA;
+                this->weights[rootA]+=this->weights[rootB];
+            }
+            else{
+                // Otherwise
+                // 1. Make rootB as the parent of rootA
+                // 2. Increment the weight of rootB by rootA's weight
+                this->parents[rootA]=rootB;
+                this->weights[rootB]+=this->weights[rootA];
+            }
+        }
+        
+        int Find(int a){
+            //traverse all the way to top to return the root node
+            while(a!=this->parents[a]){
+                //path compression
+                // a's grandparent is now a's parent
+                this->parents[a]=this->parents[parents[a]];
+                a=this->parents[a];
+            }
+            return a;
+        }
+        
+        //returns true if both a and b belongs to the same set
+        bool isInSameGroup(int a, int b){
+            return Find(a)==Find(b);
+        }
+        
+      
+        
+    };
+
+class Solution
+{
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        DisjointSet *set = new DisjointSet(n);
+        
+        int count = n;
+        
+        for(int i=0;i<edges.size();i++){
+            int a=edges[i][0];
+            int b=edges[i][1];
+            if(!set->isInSameGroup(a,b)){
+                count--;
+                 set->Union(a,b);
+            }
+           
+        }
+        return count;
+        
+    }
+};
+
+int main()
+{
+    Solution obj;
+   
+    vector<vector<int>> edges = {{0,1},{0,2},{0,3}};
+    int n=5;
+    cout<<obj.countComponents(n,edges);
+    
+
+    return 0;
+}
