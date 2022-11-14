1b5d558dee16947d231fbae8a5e8f28c0186069b
Smriti Gupta <55406606+SmritiUMD@users.noreply.github.com>
2022-11-13 23:13:04 -0800

    Update segmentTree.cpp

--- CppClasses/segmentTree.cpp
+++ CppClasses/segmentTree.cpp
@@ -1,4 +1,5 @@
 /*
+Article - https://leetcode.com/articles/a-recursive-approach-to-segment-trees-range-sum-queries-lazy-propagation/
 We will use the array tree[] to store the nodes of our segment tree (initialized to all zeros). The following scheme (0 - based indexing) is used:
 
 The node of the tree is at index 00. Thus tree[0] is the root of our tree.
@@ -26,3 +27,33 @@ void buildSegTree(vector<int>& arr, int treeIndex, int lo, int hi)
 
 // call this method as buildSegTree(arr, 0, 0, n-1);
 // Here arr[] is input array and n is its size.
+
+
+//Read/Query on segment tree
+int querySegTree(int treeIndex, int lo, int hi, int i, int j)
+{
+    // query for arr[i..j]
+
+    if (lo > j || hi < i)               // segment completely outside range
+        return 0;                       // represents a null node
+
+    if (i <= lo && j >= hi)             // segment completely inside range
+        return tree[treeIndex];
+
+    int mid = lo + (hi - lo) / 2;       // partial overlap of current segment and queried range. Recurse deeper.
+
+    if (i > mid)
+        return querySegTree(2 * treeIndex + 2, mid + 1, hi, i, j);
+    else if (j <= mid)
+        return querySegTree(2 * treeIndex + 1, lo, mid, i, j);
+
+    int leftQuery = querySegTree(2 * treeIndex + 1, lo, mid, i, mid);
+    int rightQuery = querySegTree(2 * treeIndex + 2, mid + 1, hi, mid + 1, j);
+
+    // merge query results
+    return merge(leftQuery, rightQuery);
+}
+
+// call this method as querySegTree(0, 0, n-1, i, j);
+// Here [i,j] is the range/interval you are querying.
+// This method relies on "null" nodes being equivalent to storing zero.
